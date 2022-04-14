dbbc4d42fb1a8a4a11f0f80c581fa63d1f497363
Seong Ug Kim <sukim2017@gmail.com>
2022-04-13 21:09:56 -0400

    Restaurant 1.1.3

--- src/main/java/edu/emory/restaurant/RestaurantApplication.java
+++ src/main/java/edu/emory/restaurant/RestaurantApplication.java
@@ -1,23 +1,11 @@
 package edu.emory.restaurant;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import edu.emory.restaurant.domain.Restaurant;
-import edu.emory.restaurant.service.CompanyService;
 import edu.emory.restaurant.service.JsonService;
-import edu.emory.restaurant.vo.Company;
-import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
 public class RestaurantApplication {
